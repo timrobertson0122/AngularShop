0023a12c04f9cb3b1cac4d294906182b4a1fffe7
Tim <timrobertson0122@gmail.com>
2015-08-18 15:01:39 +0100

    first unit test passing - displays list of products

---
+++ public/js/shopController.js
@@ -0,0 +1,84 @@
+app.controller('ShopController', function() {
+  this.products = items;
+});
+
+var items = [
+  { name: "Suede Shoes, Blue", 
+    price: 42.00, 
+    quantity: 4, 
+    category: "Womens Footwear",
+    image: ""
+  },
+  { name: "Almond Toe Court Shoes, Patent Black", 
+    price: 99.00,
+    quantity: 5, 
+    category: "Womens Footwear", 
+    image: ""
+  },
+  { name: "Leather Driver Saddle Loafers, Tan", 
+    price: 34.00,
+    quantity: 12, 
+    category: "Mens Footwear", 
+    image: ""
+  },
+  { name: "Flip Flops, Red", 
+    price: 19.00, 
+    quantity: 6, 
+    category: "Mens Footwear",
+    image: ""
+  },
+  { name: "Flip Flops, Blue", 
+    price: 19.00, 
+    quantity: 0, 
+    category: "Mens Footwear",
+    image: ""
+  },
+  { name: "Gold Button Cardigan, Black", 
+    price: 167.00,
+    quantity: 6, 
+    category: "Womens Casual", 
+    image: ""
+  },
+  { name: "Cotton Shorts, Medium Red", 
+    price: 30.00,
+    quantity: 5, 
+    category: "Womens Casual", 
+    image: ""
+  },
+  { name: "Fine Stripe Short Sleeve Shirt, Grey", 
+    price: 49.99,
+    quantity: 9, 
+    category: "Mens Casual", 
+    image: ""
+  },
+  { name: "Fine Stripe Short Sleeve Shirt, Green",
+    price: 39.99, 
+    quantity: 3, 
+    category: "Mens Casual", 
+    image: ""
+  },
+  { name: "Bird Print Dress, Black", 
+    price: 270.00,
+    quantity: 10, 
+    category: "Womens Formal", 
+    image: ""
+  },
+  { name: "Mid Twist Cut-Out Dress, Pink", 
+    price: 540.00,
+    quantity: 5, 
+    category: "Womens Formal", 
+    image: ""
+  },
+  { name: "Sharkskin Waistcoat, Charcoal", 
+    price: 75.00,
+    quantity: 6, 
+    category: "Mens Formal", 
+    image: ""
+  },
+  { name: "Lightweight Patch Pocket Blazer, Deer", 
+    price: 175.50, 
+    quantity: 1, 
+    category: "Mens Formal", 
+    image: ""
+  }
+];
