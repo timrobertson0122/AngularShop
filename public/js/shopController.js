83505e3957dc9c76d27048a7ac96244743d16fb2
Tim <timrobertson0122@gmail.com>
2015-08-18 16:00:41 +0100

    images added to products

--- public/js/shopController.js
+++ public/js/shopController.js
@@ -7,78 +7,106 @@ var items = [
     price: 42.00, 
     quantity: 4, 
     category: "Womens Footwear",
-    image: ""
+    images: [
+      "public/images/blue-suede-shoes-2.jpg",
+      "public/images/blue-suede-shoes-3.jpg",
+      "public/images/blue-suede-shoes-4.jpg"
+      ]
   },
   { name: "Almond Toe Court Shoes, Patent Black", 
     price: 99.00,
     quantity: 5, 
     category: "Womens Footwear", 
-    image: ""
+    images: [
+      "public/images/almond-toe-court-shoes.jpeg"
+      ]
   },
   { name: "Leather Driver Saddle Loafers, Tan", 
     price: 34.00,
     quantity: 12, 
     category: "Mens Footwear", 
-    image: ""
+    images: [
+      "public/images/leather-driver-saddle-loafers.jpg"
+      ]
   },
   { name: "Flip Flops, Red", 
     price: 19.00, 
     quantity: 6, 
     category: "Mens Footwear",
-    image: ""
+    images: [
+      "public/images/flip-flops.jpg"
+      ]
   },
   { name: "Flip Flops, Blue", 
     price: 19.00, 
     quantity: 0, 
     category: "Mens Footwear",
-    image: ""
+    images: [
+      "public/images/flip-flops-blue.jpg"
+      ]
   },
   { name: "Gold Button Cardigan, Black", 
     price: 167.00,
     quantity: 6, 
     category: "Womens Casual", 
-    image: ""
+    images: [
+      "public/images/gold-button-cardigan.jpg"
+      ]
   },
   { name: "Cotton Shorts, Medium Red", 
     price: 30.00,
     quantity: 5, 
     category: "Womens Casual", 
-    image: ""
+    images: [
+      "public/images/cotton-shorts-red.jpeg"
+      ]
   },
   { name: "Fine Stripe Short Sleeve Shirt, Grey", 
     price: 49.99,
     quantity: 9, 
     category: "Mens Casual", 
-    image: ""
+    images: [
+      "public/images/short-sleeve-shirt-grey.jpeg"
+      ]
   },
   { name: "Fine Stripe Short Sleeve Shirt, Green",
     price: 39.99, 
     quantity: 3, 
     category: "Mens Casual", 
-    image: ""
+    images: [
+      "public/images/short-sleeve-shirt-green.jpg"
+      ]
   },
   { name: "Bird Print Dress, Black", 
     price: 270.00,
     quantity: 10, 
     category: "Womens Formal", 
-    image: ""
+    images: [
+      "public/images/bird-print-dress-black.jpeg"
+      ]
   },
   { name: "Mid Twist Cut-Out Dress, Pink", 
     price: 540.00,
     quantity: 5, 
     category: "Womens Formal", 
-    image: ""
+    images: [
+      "public/images/cut-out-dress-pink.jpeg"
+      ]
   },
   { name: "Sharkskin Waistcoat, Charcoal", 
     price: 75.00,
     quantity: 6, 
     category: "Mens Formal", 
-    image: ""
+    images: [
+      "public/images/waistcoat-grey.jpg"
+      ]
   },
   { name: "Lightweight Patch Pocket Blazer, Deer", 
     price: 175.50, 
     quantity: 1, 
     category: "Mens Formal", 
-    image: ""
+    images: [
+      "public/images/blazer-deer.jpeg"
+      ]
   }
 ];
