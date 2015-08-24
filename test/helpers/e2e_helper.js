a06ea2397645841914b4113a2c14d071699a28bb
Tim <timrobertson0122@gmail.com>
2015-08-24 11:46:04 +0100

    removed redundant helper file, fixed position of shopping basket :)

--- test/helpers/e2e_helper.js
+++
@@ -1,4 +0,0 @@
-module.exports.scrollElemFinderIntoView = function(elemFinder) {
-  var promise = browser.executeScript('arguments[0].scrollIntoView(false)', elemFinder.getWebElement());
-  return promise;
-};
